41e0842df86b891ef1e0562b139222672c389530
Armin joyaian <137637877+Arminjoyaian@users.noreply.github.com>
2023-07-15 23:42:39 +0330

    Delete Gmail.py

--- Gmail.py
+++
@@ -1,9 +0,0 @@
-import qrcode
-
-qr = qrcode.make("kiyakeynia@gmail.com")
-
-qr.save("Assignment 9/qrcode.png")
-
-#qr = segno.make("kiyakeynia@gmail.com")
-
-#qr.save('Assignment 9/qrcode.png', dark='darkblue', light='lightblue', scale=10)
