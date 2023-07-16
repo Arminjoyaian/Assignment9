28855fd29cf12d5d988c5919579ba0c1c9110172
Armin joyaian <137637877+Arminjoyaian@users.noreply.github.com>
2023-07-16 11:30:14 +0330

    Delete Gmail.py

--- Gmail.py
+++
@@ -1,5 +0,0 @@
-from colorama import Fore, Back
-print(Back.LIGHTRED_EX, "Arminjoya1442@gmail.com", Back.RESET)
-
-from colorama import Fore, Back
-print(Back.MAGENTA, "Arminjoya1442@gmail.com", Back.RESET)
